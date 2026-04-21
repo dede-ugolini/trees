18faff4b407479117c7b40c5b91d6ede7b24e8a3
Andre Ugolini <andreugolini@unochapeco.edu.br>
2026-04-21 14:12:53 -0300

    Post-order-walk
    Tranverses the tree in post-order (left, right, root).

--- rbt/rbt.hpp
+++ rbt/rbt.hpp
@@ -192,6 +192,17 @@ template <typename T> void pre_order_walk(Node<T> *&root) {
   in_order_walk(root->right);
 }
 
+template <typename T> void post_order_walk(Node<T> *&root) {
+  if (root == NIL<T>()) {
+    return;
+  }
+  in_order_walk(root->left);
+  in_order_walk(root->right);
+  root->color == RED
+      ? std::cout << COLOR_RED << root->data << COLOR_RESET << std::endl
+      : std::cout << root->data << std::endl;
+}
+
 template <typename T> void purge(Node<T> *&root) {
   if (root != NIL<T>()) {
     purge(root->left);
